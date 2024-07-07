47ee126ad9eedbc6f1511a13ca666a309b5a63a1
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2024-07-07 17:51:04 +0300

    Добавьте файлы проекта.

---
+++ switch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console .ReadLine());
+
+            switch (a)
+            {
+                case 0:
+                    Console.WriteLine("Вы ввели 0");
+                    break;
+                case 1: 
+                    Console.WriteLine("Вы ввели 1");
+                    break;
+                default: 
+                    Console.WriteLine("Введите 0 или 1");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ учу сишарп/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace учу_сишарп
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b;
+
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+
+            Console.WriteLine("Сложение двух чисел");
+
+            try
+            {
+                Console.WriteLine("Введите число 1");
+                a = float.Parse(Console.ReadLine(), numberFormatInfo);
+                 
+                Console.WriteLine("Введите число 2");
+                b = float.Parse(Console.ReadLine(), numberFormatInfo);
+
+                Console.WriteLine("Результат " + (a + b));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка конвертации");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

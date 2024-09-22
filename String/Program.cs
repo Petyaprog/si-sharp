a96e519f1410a4e20490cd97a7729d73d043fcfe
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2024-09-22 15:53:13 +0300

    добавил проект

---
+++ String/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку используя только русские буквы");
+            string str = Console.ReadLine().ToLower();
+            
+            char[] A = { 'я', 'е', 'ё', 'у', 'ы', 'а', 'и', 'о', 'ю', 'э' };
+            char[] B = {'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'};
+
+            string[] words = str.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> uniqueWords = new List<string>();
+
+            foreach (string word in words)
+            {
+                if (!uniqueWords.Contains(word) && A.Contains(word[0]))
+                {
+                    uniqueWords.Add(word);
+                }
+            }
+            
+            foreach (string word in words)
+            {
+                if (!uniqueWords.Contains(word) && B.Contains(word[0]))
+                {
+                    uniqueWords.Add(word);
+                }          
+            }
+
+            string output = string.Join(" ", uniqueWords);
+
+            Console.WriteLine(output);
+
+            Console.ReadLine();
+        }
+    }
+}

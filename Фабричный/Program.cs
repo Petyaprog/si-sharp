8ed95cd02b53a04d3eacae107cd346620e5cc0e5
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2024-11-14 13:35:21 +0300

    доделал

--- Фабричный/Program.cs
+++ Фабричный/Program.cs
@@ -11,13 +11,22 @@ namespace Фабричный
 
             string s1, s2, s3, s4, s5;
             s1 = "veevev";
-            s2 = "evev";
+            s2 = "rrrrtt";
             s3 = "evvev";
-            s4 = "evvvev";
+            s4 = "qwqwqw";
             s5 = "rbrbrr";
             Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.WriteLine();
             
             Console.WriteLine(creator1.AnOperation(s1));
+            Console.WriteLine(creator1.AnOperation(s2));
+            Console.WriteLine(creator2.AnOperation(s3));
+            Console.WriteLine(creator2.AnOperation(s4));
+            Console.WriteLine(creator1.AnOperation(s5));
         }
     }
 }
